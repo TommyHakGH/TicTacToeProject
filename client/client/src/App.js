8859ab1e360d20221e47543fdc57b290dcff6148
Tommy Hak <thak@albany.edu>
2023-05-14 06:35:38 -0400

    edit backend server, created function to grab information from user in front end and generating and storing acccount info in stream. added const declarations to make unique user id. Added function to create tokens for user information. Added signup functions for signup button.

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import Login from "./components/Login";
 import SignUp from "./components/Signup";
+import { StreamChat } from 'stream-chat';
 
 function App() {
+  const api_key = process.env.APIKEY
+  
+  const client = StreamChat.getInstance()
+
   return (
     <div className="App">
       <SignUp />
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { StreamChat } from "stream-chat"; 
+import {v4 as uuidv4} from "uuid";
+import bcrypt from "bcrypt";
 require('dotenv').config( {path: '/custom/path/.env'} );
 
 
@@ -10,8 +12,21 @@ app.use(cors());
 app.use(express.json());
 const api_key = process.env.APIKEY
 const api_secret = process.env.APISECRET
+const serverClient = StreamChat.getInstance(api_key, api_secret);
 
-const serverClient = new StreamChat.getInstance(api_key, api_secret);
+app.post("/signup", async (req, res) => {
+    try{
+    const {firstName, lastName, username, password} = req.body
+    const userId = uuidv4();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const token = serverClient.createToken(userId);
+    resizeTo.json({token, userId, firstName, lastName, username, hashedPassword});
+    } catch(error) {
+      res.json(error);  
+    }
+});
+
+app.post("/login")
 app.listen(3001, () => {
     console.log("Server is running on port 3001")
 });
