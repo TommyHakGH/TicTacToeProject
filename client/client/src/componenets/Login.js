246173a2d8002f5ef988236d49632ff6d74e7169
Tommy Hak <thak@albany.edu>
2023-05-13 23:25:38 -0400

    added username, and password inputs towards the login page

--- client/client/src/componenets/Login.js
+++ client/client/src/componenets/Login.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 function Login() {
+
+
   return (
-    <div>Login</div>
+    <div className = "signUp">
+        <label>Log In</label>
+        <input placeholder= "Username" onChange={(event) => {
+            setUser({...user, username: event.target.value})
+        }} />
+        <input placeholder= "Password" onChange={(event) => {
+            setUser({...user, password: event.target.value})
+        }} />
+        <button onClick ={signUp}> Sign Up 
+
+        </button>
+
+    </div>
   )
 }
 
